419a4939968a912b7a790d13f2a55f16a073f56e
Muhammad Akif <Muhammad-Akif@outlook.com>
2021-04-22 02:59:17 +0500

    updated

--- src/components/LoginComp.jsx
+++ src/components/LoginComp.jsx
@@ -110,10 +110,6 @@ export default function SignIn() {
             id="password"
             autoComplete="current-password"
           />
-          {/* <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          /> */}
           <Button
             type="submit"
             fullWidth
