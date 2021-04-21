9580a9481621baf119f0bc6a43d464bba684b563
Muhammad Akif <Muhammad-Akif@outlook.com>
2021-04-21 17:25:30 +0500

    updated

--- src/components/LoginComp.jsx
+++ src/components/LoginComp.jsx
@@ -129,7 +129,7 @@ export default function SignIn() {
             </Grid>
             <Grid item>
               <NavLink to="/signup" variant="body2">
-                Don't have account? SignUp
+                No account? Sign Up
               </NavLink>
             </Grid>
           </Grid>
