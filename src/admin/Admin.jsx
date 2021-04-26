a21297b544324cf9ced5e8567fce8096950223b4
Muhammad Akif <Muhammad-Akif@outlook.com>
2021-04-26 21:30:57 +0500

    upated admin

--- src/admin/Admin.jsx
+++ src/admin/Admin.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import StylishSidebar from './Sidebar'
 
 export default function Admin() {
     return (
         <div>
-            Hi
+            <StylishSidebar/>
         </div>
     )
 }
