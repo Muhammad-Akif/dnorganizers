b79b1456831d4aee783dd21381454dc812649923
Muhammad Akif <Muhammad-Akif@outlook.com>
2021-05-11 23:44:56 +0500

    updated user Packages

--- src/user UI/componenets/UserCard.jsx
+++ src/user UI/componenets/UserCard.jsx
@@ -4,12 +4,12 @@ export default function UserCard(props) {
   return (
     <div className="card control-width">
       <div className="text-center">
-        <strong style={{ fontSize: '1.5rem' }}> {props.name} </strong>
+        <strong style={{ fontSize: '2rem' }}> {props.name} </strong>
       </div>
-      <ul className="list-group list-group-flush text-center">
+      <ul className="list-group list-group-flush text-center" style={{ fontSize: '1.25rem' }}>
         <li className="list-group-item">{props.theme}</li>
         <li className="list-group-item">{props.venu}</li>
-        <li className="list-group-item">{props.menu}</li>
+        <li className="list-group-item">{props.menu.join(", ")}</li>
         <li className="list-group-item">Rs{props.price}</li>
       </ul>
       <button type="button" class="btn btn-success Cbtn">BOOK NOW</button>
