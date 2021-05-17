876181f27a3e2b3cbe835d50fafee6b83e959e5e
Muhammad Akif <Muhammad-Akif@outlook.com>
2021-05-17 20:21:15 +0500

    card updated

--- src/user UI/componenets/UserCard.jsx
+++ src/user UI/componenets/UserCard.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 export default function UserCard(props) {
   return (
     <div className="card control-width">
-      <div className="text-center">
-        <strong style={{ fontSize: '2rem' }}> {props.name} </strong>
+      <div style={{padding:"5px 0px",borderBottom:"1px solid black"}} className="text-center">
+        <strong style={{ fontSize: '3rem',fontFamily: "'Sacramento', Arial, serif"}}> {props.name} </strong>
       </div>
-      <ul className="list-group list-group-flush text-center" style={{ fontSize: '1.25rem' }}>
-        <li className="list-group-item">{props.theme}</li>
-        <li className="list-group-item">{props.venu}</li>
-        <li className="list-group-item">{props.menu.join(", ")}</li>
-        <li className="list-group-item">Rs{props.price}</li>
+      <ul className="list-group list-group-flush text-center" style={{ fontSize: '1.35rem' }}>
+        <li className="fld list-group-item">{props.theme}</li>
+        <li className="fld list-group-item">{props.venu}</li>
+        <li className="fld list-group-item">{props.menu.join(", ")}</li>
+        <li className="fld list-group-item">Rs{props.price}</li>
       </ul>
       <button type="button" class="btn Cbtn">BOOK NOW</button>
     </div>
