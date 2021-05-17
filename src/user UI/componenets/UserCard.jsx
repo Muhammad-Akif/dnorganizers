cc12a0ada5c849521b6cc8b9ca6274931f6f583b
Muhammad Akif <Muhammad-Akif@outlook.com>
2021-05-17 20:40:52 +0500

    card ready

--- src/user UI/componenets/UserCard.jsx
+++ src/user UI/componenets/UserCard.jsx
@@ -10,7 +10,7 @@ export default function UserCard(props) {
         <li className="fld list-group-item">{props.theme}</li>
         <li className="fld list-group-item">{props.venu}</li>
         <li className="fld list-group-item">{props.menu.join(", ")}</li>
-        <li className="fld list-group-item">Rs{props.price}</li>
+        <li className="fld list-group-item" style={{ fontSize: '2.2rem',fontFamily: "serif"}}><strong>Rs{props.price}</strong></li>
       </ul>
       <button type="button" class="btn Cbtn">BOOK NOW</button>
     </div>
